98e292b4889748fb618efb3bd422efc3f5ed592f
JungYuCheol <cokr3430@gmail.com>
2016-02-01 17:42:33 +0000

    minor fix

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -13,7 +13,7 @@ class StudyController < ApplicationController
     studee.update(deposit: studee.deposit - new_his.get_fine, absence: studee.absence + absence, late: studee.late + late, homework: studee.homework + homework, voca: studee.voca + voca)
     
     respond_to do |format|
-      format.json { render json: {id: studee.id, deposit: studee.deposit, his: new_his, fine: new_his.get_fine}}
+      format.json { render json: {id: studee.id, deposit: studee.deposit, his: new_his, fine: new_his.get_fine, date: new_his.created_at.in_time_zone("Seoul").strftime("%Y-%m-%d %a")}}
     end
   end
   
